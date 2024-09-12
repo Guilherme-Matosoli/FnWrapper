36f07776712f1a385aa0c6643daf3ff937d6675a
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-09-12 11:54:17 -0300

    :sparkles: feat(commands/howami): Introduce `whoami` command to retrieve user information.

--- internal/commands/howami.go
+++ internal/commands/howami.go
@@ -1,1 +1,16 @@
 package commands
+
+import (
+  "github.com/spf13/cobra"
+  "github.com/Guilherme-Matosoli/FnWrapper/internal/handlers"
+)
+
+
+func howamiCommand() *cobra.Command{
+  command := &cobra.Command{
+    Use: "whoami",
+    Run: handlers.HowamiHandler,
+  }
+
+  return command
+}
